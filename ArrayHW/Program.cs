9fe2827e1e509df41fc29d507efc8bc3fc8115ac
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-24 22:01:01 +0300

    Add project files.

---
+++ ArrayHW/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 5, 7, 10, 12, 15, 10, 5 };
+            int min = arr[0];
+            int counter = 0;
+            
+            foreach (var item in arr)
+            {
+                if (item < min)
+                    min = item;
+            }
+            
+            foreach (var item in arr)
+            {
+                if (item == min + 5)
+                    counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
