827c3e8f28a92497b69cdaa3ef9ded3130f6653a
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-28 16:47:29 +0300

    Implement Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 0, 4, 2, 4, 4, 5, 6, 10, 8, 9 };
+            int res = 0, counter = 0;
+
+            for (int i = 1; i < arr.Length; i+=2)
+            {
+                if (arr[i]%2==0)
+                {
+                    res += arr[i];
+                    counter++;
+                }
+            }
+            Console.WriteLine($"Average value: {res/counter}");
+        }
+    }
+}
